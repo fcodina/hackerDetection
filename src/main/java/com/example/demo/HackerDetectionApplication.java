a6e93b2b00c33e8ce838ecf1c98b34f6435cffb8
francesc <fcodina@gmail.com>
2021-03-31 23:00:05 +0200

    Initial commit Spring boot.

---
+++ src/main/java/com/example/demo/HackerDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerDetectionApplication.class, args);
+	}
+
+}
