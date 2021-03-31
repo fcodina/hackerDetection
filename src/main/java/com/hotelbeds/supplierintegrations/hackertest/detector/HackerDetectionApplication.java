0de996c5ad343f08ba0544289c4aadaf6d860420
francesc <fcodina@gmail.com>
2021-03-31 23:03:50 +0200

    Initial commit Spring boot.

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectionApplication.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectionApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.hotelbeds.supplierintegrations.hackertest.detector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
